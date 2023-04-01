89335a552af4126a0a30cb03c76dacf763d39056
gpchandrarao <gandikotapoornachandrarao@gmail.com>
2023-04-01 09:30:34 +0000

    Covid-19 India Portal

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+
+const path = require("path");
+const dbPath = path.join(__dirname, "todoApplication.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server is running at http:/localhost:3000/");
+    });
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+initializeDBAndServer();
+
+module.exports = app;
